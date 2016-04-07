be2205409fdf2745ae95e8fe99be9bff6a656ac7
LeBezout <lebezout@gmail.com>
2016-04-07 21:34:06 +0200

    HtmlReportFastGardenVisitor : affichage du nombre total de combats + erreur si on veut générer un rapport vide

--- leekwars-utils/src/main/java/com/leekwars/utils/fastgarden/impl/HtmlReportFastGardenVisitor.java
+++ leekwars-utils/src/main/java/com/leekwars/utils/fastgarden/impl/HtmlReportFastGardenVisitor.java
@@ -7,7 +7,6 @@ import java.nio.file.Files;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.LinkedList;
-import java.util.List;
 import java.util.Map;
 
 import com.leekwars.utils.LWUtils;
