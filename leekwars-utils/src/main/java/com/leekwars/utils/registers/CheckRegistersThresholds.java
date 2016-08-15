533a8aa6b72b6dc9adbf62ffb587d0aa336b067e
Bezout <lebezout@gmail.com>
2016-08-15 12:02:17 +0200

    Correction javadoc

--- leekwars-utils/src/main/java/com/leekwars/utils/registers/CheckRegistersThresholds.java
+++ leekwars-utils/src/main/java/com/leekwars/utils/registers/CheckRegistersThresholds.java
@@ -7,7 +7,7 @@ import com.leekwars.utils.model.KeyValueCouple;
 import com.leekwars.utils.model.LeekSummary;
 
 /**
- * Classe outils pour détections le remplissages des registres. Pour info :<ul>
+ * Classe outil pour la détection des remplissages des registres. Pour info :<ul>
  * <li>Les registres sont limités à 100 éléments.</li>
  * <li>La clé, chaîne qui doit contenir 100 caractères au maximum.</li>
  * <li>La valeur, chaîne qui doit contenir 5000 caractères au maximum.</li>
@@ -16,7 +16,7 @@ import com.leekwars.utils.model.LeekSummary;
  */
 public abstract class CheckRegistersThresholds {
 	
-	/** capacité maximul des registres pour 1 poireau */
+	/** capacité maximale des registres pour 1 poireau */
 	public static final int REGISTERS_MAX_CAPACITY = 100;
 
 	/**
