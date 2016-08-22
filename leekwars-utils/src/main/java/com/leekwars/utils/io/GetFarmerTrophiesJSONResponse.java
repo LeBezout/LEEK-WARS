6d80b884fa7f37f82289de543f18a2bcb4624c15
Bezout <lebezout@gmail.com>
2016-08-22 22:45:17 +0200

    debut impl getFarmerTrophies

---
+++ leekwars-utils/src/main/java/com/leekwars/utils/io/GetFarmerTrophiesJSONResponse.java
@@ -0,0 +1,10 @@
+package com.leekwars.utils.io;
+
+import com.leekwars.utils.model.SimpleJSONResponse;
+
+/**
+ * Informations recues suite à un appel à "trophy/get-farmer-trophies".
+ * @author Bezout
+ */
+public class GetFarmerTrophiesJSONResponse extends SimpleJSONResponse {
+}
