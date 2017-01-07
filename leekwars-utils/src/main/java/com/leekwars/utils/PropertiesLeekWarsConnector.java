56577116a9273a84ad0c8284561ed2879a3f7804
Bezout <lebezout@gmail.com>
2017-01-07 10:05:29 +0100

    Bezout: version 1.3 : import inutile

--- leekwars-utils/src/main/java/com/leekwars/utils/PropertiesLeekWarsConnector.java
+++ leekwars-utils/src/main/java/com/leekwars/utils/PropertiesLeekWarsConnector.java
@@ -2,7 +2,6 @@ package com.leekwars.utils;
 
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.Properties;
 
